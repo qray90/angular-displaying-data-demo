690c370df1ef083942eef803a15e4140fa15f756
ray <qray@foxmail.com>
2020-02-11 22:57:41 +0800

    use inline template to display data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{ title }}</h1>
+    <h2>My favorite hero is: {{ myHero }}</h2>
+    <p>heroes:</p>
+    <ul>
+      <li *ngFor="let hero of heroes">
+        {{ hero.name }}
+      </li>
+    </ul>
+
+    <p *ngIf="heroes.length > 3">there are many heroes!</p>
+  `
 })
 export class AppComponent {
-  title = 'angular-displaying-data';
+  title = 'Tour of Heroes';
+  heroes = [
+    new Hero(1, 'Windstorm'),
+    new Hero(13, 'Bombasto'),
+    new Hero(15, 'Magneta'),
+    new Hero(20, 'Tornado')
+  ];
+  myHero = this.heroes[0];
 }
