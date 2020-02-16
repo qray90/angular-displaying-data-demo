85a1319cab2531fed943c65a495084883ab81659
ray <qray@foxmail.com>
2020-02-16 18:49:49 +0800

    rxjs - operators

--- src/app/rx-example/rx-example.component.ts
+++ src/app/rx-example/rx-example.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { fromEvent } from 'rxjs';
+import { fromEvent, of, pipe } from 'rxjs';
 
 import { ajax } from 'rxjs/ajax';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rx-example',
@@ -28,14 +29,30 @@ export class RxExampleComponent implements OnInit {
     // ajax调用
     const apiData = ajax('https://api.apiopen.top/musicDetails?id=604392760');
 
-    apiData.subscribe(({ status, response}) => {
+    apiData.subscribe(({ status, response }) => {
       console.log(status, response);
     });
 
+    // 操作符
+    // of(1, 2, 3, 4, 5)
+    //   .pipe(
+    //     filter(n => n % 2 === 0),
+    //     map(n => n * n)
+    //   )
+    //   .subscribe(x => console.log(x));
+
+    const nums = of(1, 2, 3, 4, 5);
+    const squareOddVals = pipe(
+      filter((n: number) => n % 2 !== 0),
+      map(n => n * n)
+    );  // 返回新函数？？
+
+    const squareOdd = squareOddVals(nums); // 函数式编程？？？？
+
+    squareOdd.subscribe(x => console.log(x));
   }
 
   clickRxBox(e) {
     console.log(e.clientX, e.clientY, e.offsetY);
   }
-
 }
